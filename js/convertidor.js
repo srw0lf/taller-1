d15ff55fc3b2e93f5aba15d3913e7d8f39611be2
Juan David Gaspar <juanchito292014@gmail.com>
2022-08-23 12:31:47 -0500

    kjljk

--- js/convertidor.js
+++ js/convertidor.js
@@ -1,8 +1,8 @@
 function convertir(){
-    let num1=parseInt(document.getElementById("num1").value);
-    let num2=parseInt(document.getElementById("num2").value);
-    let op=document.getElementById("operacion").value;
-    if(op==1){
+    let num=parseInt(document.getElementById("num").value);
+    let op=document.getElementById("opcion").value;
+    let op2=document.getElementById("opcion2").value;
+    if(op==1 && op2==2){
         let res=num1+num2;
         alert(`El numero 1 es ${num1} y el numero 2 ${num2} la suma es ${res}`)
     }
